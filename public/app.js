7f7a008c0e5d3e0641b9e3763d9d0dffce27a891
Dylan Zincone <dylan.zincone@gmail.com>
2015-08-27 11:31:41 -0600

    Updated to get data

--- public/app.js
+++ public/app.js
@@ -1,6 +1,20 @@
-var app = angular.module('newApp', []);
+var app = angular.module('newApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/posts.html',
+        controller: 'ApplicationController'
+      })
+
+    $locationProvider.html5Mode(true)
+  }]);
 
 
-app.controller("HomeController", function(){
-  
+app.controller("ApplicationController", function($scope, $http){
+  $scope.name = "Dylan",
+  $http.get('/posts').then(function(data){
+    $scope.data = data
+  })
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/posts-workshop')
+var Post = db.get('posts')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/posts', function(req, res, next){
+  Post.find().then(function(data){
+    console.log(data)
+    res.json(data)
+  })
+})
 module.exports = router;
